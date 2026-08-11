34a58edfca6b2554d193f0b44a02c0bca094cfc4
abhinav <abhinav@kloudwin.com>
2026-08-11 10:52:35 +0530

    feat: add remote image patterns for Gravatar and GitLab

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "avatars.githubusercontent.com" },
       { protocol: "https", hostname: "github.com" },
+      { protocol: "https", hostname: "secure.gravatar.com" },
+      { protocol: "https", hostname: "www.gravatar.com" },
+      { protocol: "https", hostname: "gravatar.com" },
+      { protocol: "https", hostname: "gitlab.com" },
+      { protocol: "https", hostname: "*.gitlabstatic.com" },
     ],
   },
 };
